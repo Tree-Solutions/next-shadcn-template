b299e7259491ca5549b0913788085fcd508bc182
skrypka-mark <mark.skrypka@gmail.com>
2025-07-23 14:13:43 +0200

    feat: integrate React Query by adding RootProvider and query client setup

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { getQueryClient } from "@/lib/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+export default function RootProvider({ children }: React.PropsWithChildren) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
